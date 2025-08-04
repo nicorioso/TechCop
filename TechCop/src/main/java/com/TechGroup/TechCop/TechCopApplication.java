37c2c418b33cb1e1f590560e6bfa4a54dc421239
Admin <nicoriosops4@gmail.com>
2025-08-04 08:52:20 -0700

    se anade el proyecto al repositorio

---
+++ TechCop/src/main/java/com/TechGroup/TechCop/TechCopApplication.java
@@ -0,0 +1,13 @@
+package com.TechGroup.TechCop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechCopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechCopApplication.class, args);
+	}
+
+}
---
+++ TechCop/src/test/java/com/TechGroup/TechCop/TechCopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TechGroup.TechCop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechCopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
