e9898fa9246aee8058cd2bc85ddb3a803e94bca4
nicorioso <nicoriosops4@gmail.com>
2025-08-25 18:08:10 -0500

    se anade un archivo index, la dependencia thymeleaf, y un archivo controlador para redirigir al archivo index

---
+++ backend/src/main/java/com/techgroup/techcop/controllers/WebController.java
@@ -0,0 +1,12 @@
+package com.techgroup.techcop.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+    @GetMapping("/")
+    public String index() {
+        return "index";  // busca templates/index.html
+    }
+}
